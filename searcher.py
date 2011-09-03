2c6cc2b371a573f270b584279cbe2a4093418ebd
Anton Lashkov <lenton_91@mail.ru>
2011-09-03 14:12:48 +0400

    function for collecting artists in folder

--- searcher.py
+++ searcher.py
@@ -11,11 +11,19 @@ try:
 except:
     sys.exit()
 
-class Searcher():
+artists_list = []
 
-    def __init__(self):
-        artists_list = []
+def scan_folder(self, path):
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            filename = os.path.join(root, name)
+            if os.path.isfile(filename) and eyeD3.isMp3File(filename):
+                tag = eyeD3.Tag()
+                try:
+                    tag.link(filename)
+                    if tag.getArtist() not in artists_list:
+                        print tag.getArtist()
+                        artists_list.append(tag.getArtist())
+                except :
+                    pass
 
-    def scan_folder(self, path):
-        for songs in os.listdir(path):
-            
