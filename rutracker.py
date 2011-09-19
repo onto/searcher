6da539ae55251926061afcadb5c2f5f505c8b3df
Anton Lashkov <lenton_91@mail.ru>
2011-09-19 12:20:23 +0400

    update class

--- rutracker.py
+++ rutracker.py
@@ -4,8 +4,6 @@
 import urllib
 import urllib2
 from BeautifulSoup import BeautifulSoup
-import time
-import re
 
 class Rutracker():
 
@@ -26,32 +24,42 @@ class Rutracker():
             pass
 
     def get_forum_ids(self):
+        """
+        It return {'id':'title of section', ...} like:
+        {'1142': 'Folk, NewAge и Flamenco (DVD Video)',
+         '1788': 'Metal (DVD Video)'}
+        """
 
-        forum_ids = []
+        forum_ids = {}
 
         page = self.opener.open("http://rutracker.org/forum/tracker.php")
-        soup = BeautifulSoup(page.read())
+        soup = BeautifulSoup(page.read(), fromEncoding="utf-8")
 
         for optgroup in ["&nbsp;Музыка","&nbsp;Электронная музыка","&nbsp;Рок-музыка"]:
-            for group in soup.findAll('optgroup',label=optgroup):
+            for group in soup.findAll('optgroup', label=optgroup):
                 for option in group.findAll('option', attrs={'class' : None}):
-                    forum_ids.append(option['value'])
+                    forum_ids[option['value']] = option.text[3:-6]
 
         return forum_ids
 
     def search(self, text, ids):
+        """
+        It return result of searching in assigned forum sections like:
+        {'http://rutracker.org/forum/viewtopic.php?t=3597138':
+        u'(Punk/Hardcore/Ska-Punk)VA - Russian Tribute To Anti-Flag - 2011, MP3, V0',}
+        """
+
         links = {}
 
         for id in ids:
             link = "http://rutracker.org/forum/tracker.php?f=%s&nm=%s" % (id, text)
 
             try:
-                page = self.opener.open(link)
-                soup = BeautifulSoup(page.read())
+                page = self.opener.open(link, timeout=60)
+                soup = BeautifulSoup(page.read(), fromEncoding="utf-8")
                 for result in soup.findAll('a', attrs={'class' : 'med tLink bold'}):
-                    links[(str(result['href']))[result['href'].find('=')+1:]] = BeautifulSoup(str(result)).a.text
-
-                time.sleep(1)
+                    links["http://rutracker.org/forum/"+result['href'][2:]] = \
+                    BeautifulSoup(str(result)).a.text
             except :
                 pass
             
